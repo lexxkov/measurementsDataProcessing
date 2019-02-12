b3e29f2f452f9a5b953d3e18dcb8561309bc993b
lexxkov <lexxkov@gmail.com>
2019-02-12 19:09:52 +0200

    Added open file, create 3d array and print data by Operator number

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,29 +1,41 @@
 import numpy as np
 
 class DataProc:
-    def __init__(self, baseFilename, OpAmount, ConAmount):
+    def __init__(self, baseFilename, OpAmount, ConAmount,Repeat):
         self.baseFilename = baseFilename
         self.OpAmount = OpAmount
         self.ConAmount = ConAmount
+        self.Repeat = Repeat
 
     def open(self):
+        self.Data = np.empty([self.OpAmount,self.ConAmount,self.Repeat])
         self.datFn = self.baseFilename + ".txt"
         self.datFile = open(self.datFn, "r")
         op = 0
         con = 0
+        re = 0
+        # list = []
         for line in self.datFile:
-            while op < len(self.OpAmount):
+            if op == self.OpAmount:
+                op = 0
+                re+=1
+
+            line = line.strip()
+            self.Data[op][con][re] = float(line)
+            con+=1
+
+            if con == self.ConAmount:
                 con = 0
-                while con < len(self.ConAmount):
-                    Data[op,con].append(line)
-            op = 0
-    def printOp(OpNum=0):
-        if OpNum>=len(self.OpAmount):
+                op+=1
+
+    def printOp(self, OpNum):
+        if OpNum >= self.OpAmount:
             return "Out of Operator amount"
         else:
-            print "Operator-{}:".format(OpNum)
-            for con in self.ConAmount:
-                print "Connector-{}: {}".format(con, Data[OpNum,con])
+            print "Operator-{}: ".format(OpNum)
+            for con in range(self.ConAmount):
+                print "Connector{0}: {1}".format(con, self.Data[OpNum][con])
+                None
 
     def close(self):
             self.datFile.close()
---
+++ test_dataProcessing.py
@@ -0,0 +1,13 @@
+from dataProcessing import *
+OperatorAmount = 3
+ConnectorAmount = 10
+Repeat = 3
+Operator1=0
+Operator2=1
+Operator3=2
+filePath = '/home/alex/PythonTasks/measurementsDataProcessing/RAW/data'
+Data = DataProc(filePath, OperatorAmount , ConnectorAmount, Repeat)
+Data.open()
+Data.printOp(Operator1)
+Data.printOp(Operator2)
+Data.printOp(Operator3)
