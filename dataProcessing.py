7111571c2fe65afbd5aca73604261d91b2d4d1d8
lexxkov <lexxkov@gmail.com>
2019-02-13 09:33:00 +0200

    Added plotOp and plotCon

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class DataProc:
     def __init__(self, baseFilename, OpAmount, ConAmount,Repeat):
@@ -34,8 +35,30 @@ class DataProc:
         else:
             print "Operator-{}: ".format(OpNum)
             for con in range(self.ConAmount):
-                print "Connector{0}: {1}".format(con, self.Data[OpNum][con])
+                print "Connector{0}: {1}  Std: {2}".format(con, self.Data[OpNum][con], np.std(self.Data[OpNum][con]))
                 None
+
+    def plotOp(self, OpNum):
+        fig = plt.figure()
+        X = [i for i in range(self.ConAmount)]
+        Y = [[self.Data[OpNum][i][j] for i in range(self.ConAmount)] for j in range(self.Repeat)]
+        # F = plt.plot(lambda: X,Y[k] for k in range(self.Repeat), 'rx')
+        for j in range(self.Repeat):
+            plt.plot(X,Y[j], 'gx')
+        plt.show()
+
+    def plotCon(self, ConNum):
+        fig = plt.figure()
+        X = [i for i in range(self.OpAmount)]
+        Y = [[self.Data[i][ConNum][j] for i in range(self.OpAmount)] for j in range(self.Repeat)]
+        # F = plt.plot(lambda: X,Y[k] for k in range(self.Repeat), 'rx')
+        for j in range(self.Repeat):
+            plt.plot(X,Y[j], 'gx')
+        plt.show()
+
+
+        # ax = plt.axes([x for x in range(self.ConAmount)], [np.std(self.Data[OpNum][x]) for x in range(self.ConAmount)])
+        # ax.plot([], [], lw=2)
 
     def close(self):
             self.datFile.close()
